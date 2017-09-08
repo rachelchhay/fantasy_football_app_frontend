ee6000ab155e9c7a867e0f14c15c8292c87ff43c
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-09-08 13:53:24 -0500

    Added forms, display data, added CSS

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ const PORT      = 2000;
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
+  console.log('============================');
   console.log('Listening to port: ', PORT)
+  console.log('============================');
+
 })
